c4f76147d04c976930bffcef071683c68a4018ad
ChrisMaclean <chrismaclean39@gmail.com>
2018-06-04 16:46:39 -0500

    ex21

---
+++ ex21.py
@@ -0,0 +1,28 @@
+# this whole exercise doesn't seem that complicated really
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+print("Lets do some math with functions")
+
+age = add(23, 5)
+height = subtract(78, 6)
+weight = multiply(108, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, height: {height}, weight: {weight}, iq: {iq}")
+
+# OOO test below, nesting shit
+# what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
